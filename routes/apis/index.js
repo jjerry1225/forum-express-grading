5b679ed7e5e69892b2e5b5cb8a8f377bcdf1df9c
jjerry1225 <i183801225@gmail.com>
2023-03-16 10:25:53 +0800

    feat: add api-auth middleware

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -4,14 +4,15 @@ const passport = require('../../config/passport')
 const restController = require('../../controllers/apis/restaurant-controller')
 const userController = require('../../controllers/apis/user-controller')
 const { apiErrorHandler } = require('../../middleware/error-handler')
+const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 
 const admin = require('./modules/admin')
 
-router.use('/admin', admin)
-
+router.use('/admin', authenticated, authenticatedAdmin, admin)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 // 不用 cookie-based 驗證了，所以也不需要 Passport 幫我們建立 session，需要把這個功能關掉。
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
-router.get('/restaurants', restController.getRestaurants)
+
 router.use('/', apiErrorHandler)
 
 module.exports = router
